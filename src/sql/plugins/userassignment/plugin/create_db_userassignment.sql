2a0e6fad06e87e10e6e4f9132f9b5ced8056a26c
Guillaume Lépine <guillaume.lepine@sogeti.com>
2019-04-30 15:28:53 +0200

    correction sql

--- src/sql/plugins/userassignment/plugin/create_db_userassignment.sql
+++ src/sql/plugins/userassignment/plugin/create_db_userassignment.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS workflow_resource_user;
-
 CREATE TABLE workflow_resource_user (
 	id int AUTO_INCREMENT,
 	id_resource INT DEFAULT 0 NOT NULL,
@@ -12,6 +11,7 @@ CREATE INDEX workflow_resource_user_id_resource_fk ON workflow_resource_user(id_
 CREATE INDEX workflow_resource_user_resource_type_fk ON workflow_resource_user(resource_type);
 CREATE INDEX workflow_resource_user_id_admin_fk ON workflow_resource_user(id_user);
 
+DROP TABLE IF EXISTS workflow_task_assign_user_config;
 CREATE TABLE workflow_task_assign_user_config (
 	 id_task INT DEFAULT 0 NOT NULL ,
 	 provider_name varchar(100) default NULL,
